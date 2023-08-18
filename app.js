c0e9cd79ea904ca8246f25f9a83177516b5a25d4
DA MAC M1 105 <damacm1105@DA-Mac105s-MacBook-Air.local>
2023-08-18 15:25:48 +0200

    added porfolio

--- app.js
+++ app.js
@@ -26,11 +26,20 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+
+
+  if (n > slides.length) {
+    slideIndex = 1
+  } 
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
+
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
