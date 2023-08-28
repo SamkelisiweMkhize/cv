8fbfc1a4563b9b78a61a414c47cc9d76d4165d48
DA MAC M1 105 <damacm1105@DA-Mac105s-MacBook-Air.local>
2023-08-28 15:36:32 +0200

    formatted my code

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 
 function myScroll(id) {
-    document.getElementById(id).scrollIntoView({behavior: 'smooth'})
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' })
 }
 
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+  document.getElementById("myForm").style.display = "block";
+}
 
-  var slideIndex = 1;
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -30,19 +30,19 @@ function showSlides(n) {
 
   if (n > slides.length) {
     slideIndex = 1
-  } 
+  }
 
   if (n < 1) {
     slideIndex = slides.length
   }
 
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
 
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
